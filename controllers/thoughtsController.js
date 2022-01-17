e464a2bee008797e3327a8117036ad320a17ff53
msp4msps <msp4msps@tminus365.com>
2022-01-16 19:41:13 -0700

    add put, delete, getbyID routes

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -6,6 +6,16 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
+  getThoughtbyId(req, res) {
+    Thoughts.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then(async (thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
   createThought(req, res) {
     Thoughts.create(req.body)
       .then((thoughts) => {
@@ -26,6 +36,31 @@ module.exports = {
         console.log(err);
         res.status(500).json(err);
       });
+  },
+  updateThought(req, res) {
+    Thoughts.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : res.json(thought)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteThought(req, res) {
+    Thoughts.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json({ message: "Thought and associated reactions deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
   },
   createReaction(req, res) {
     Thoughts.findOneAndUpdate(
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,12 +2,20 @@ const router = require("express").Router();
 const {
   getThoughts,
   createThought,
+  getThoughtbyId,
   createReaction,
   deleteReaction,
+  deleteThought,
+  updateThought,
 } = require("../../controllers/thoughtsController");
 
 // /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
+router
+  .route("/:thoughtId")
+  .get(getThoughtbyId)
+  .put(updateThought)
+  .delete(deleteThought);
 router.route("/:thoughtId/reactions").post(createReaction);
 router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
